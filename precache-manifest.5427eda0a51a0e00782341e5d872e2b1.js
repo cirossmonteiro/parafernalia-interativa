080c848a817f9c5845248782279aede855845b35
Ciro Monteiro <cirossmonteiro@gmail>
2021-03-09 17:11:57 -0300

    trying github pages

---
+++ precache-manifest.5427eda0a51a0e00782341e5d872e2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcdb3ce5e3b49ef8123cf7c3aa488335",
+    "url": "./index.html"
+  },
+  {
+    "revision": "949c20444d89b6fc49fb",
+    "url": "./static/css/main.b50617ec.chunk.css"
+  },
+  {
+    "revision": "153d97025f2d3d01d262",
+    "url": "./static/js/2.e445d790.chunk.js"
+  },
+  {
+    "revision": "949c20444d89b6fc49fb",
+    "url": "./static/js/main.98ab6f22.chunk.js"
+  },
+  {
+    "revision": "787bacb5bd314db40f0d",
+    "url": "./static/js/runtime-main.8ce95b08.js"
+  }
+]);
